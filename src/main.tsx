27893b75f4d42d43c3468e045c817f3bed16a986
Ping Chen <remotenonsense@gmail.com>
2018-11-09 09:04:04 +0900

    init layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import "./main.css";
+
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="app">
+      <div className="chart">chart</div>
+      <div className="form">form</div>
+      <div className="table">table</div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: ["awesome-typescript-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },

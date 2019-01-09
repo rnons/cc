a089ef8dc67e0d6e5f185b24c9c53f0012bc8295
Ping Chen <remotenonsense@gmail.com>
2019-01-09 09:24:10 +0900

    Init draft-js

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Editor, EditorState } from "draft-js";
 
 import "./main.css";
 
 class App extends React.Component {
+  state = { editorState: EditorState.createEmpty() };
+
+  onChange = (editorState: EditorState) => this.setState({ editorState });
+
   render() {
-    return <div>hello world</div>;
+    return (
+      <div className="container pt-10">
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      </div>
+    );
   }
 }
 
